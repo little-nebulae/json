e14f50b6307e49fa5940e41c761f635754582d4d
Ryan Luong <lumintriet@gmail.com>
2026-09-21 15:38:24 +0700

    feat: define parseJsonString function.

---
+++ src/parse-json-string.ts
@@ -0,0 +1,24 @@
+import type { Result } from "@little-nebulae/result";
+import type { JSONType } from "zod";
+
+import { fail, succeed } from "@little-nebulae/result";
+
+import { InvalidJsonStringError } from "@/errors/invalid-json-string";
+
+export function parseJsonString(
+  text: string,
+): Result<JSONType, InvalidJsonStringError> {
+  try {
+    const parsedValue = JSON.parse(text);
+    return succeed(parsedValue as JSONType);
+  } catch (error) {
+    const syntaxError = error as SyntaxError;
+    return fail(
+      new InvalidJsonStringError({
+        message: "Failed to parse string as JSON.",
+        cause: syntaxError,
+        meta: null,
+      }),
+    );
+  }
+}
