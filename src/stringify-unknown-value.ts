dbc50743016bef58a38349c17bd97bc16deb9663
Ryan Luong <lumintriet@gmail.com>
2026-09-21 15:50:40 +0700

    feat: define stringifyUnknownValue function.

---
+++ src/stringify-unknown-value.ts
@@ -0,0 +1,28 @@
+import type { JSONType } from "zod";
+
+import { fail, succeed } from "@little-nebulae/result";
+
+import { InvalidJsonValueError } from "@/errors/invalid-json-value";
+
+export function stringifyUnknownValue({
+  value,
+  space = 2,
+}: {
+  value: JSONType;
+  space?: string | number;
+}) {
+  try {
+    const stringifiedText = JSON.stringify(value, null, space);
+    return succeed(stringifiedText);
+  } catch (error) {
+    const typeError = error as TypeError;
+    return fail(
+      new InvalidJsonValueError({
+        message:
+          "Failed to convert value to a JSON string because it's not serializable.",
+        cause: typeError,
+        meta: null,
+      }),
+    );
+  }
+}
