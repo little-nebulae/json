d3f2674c0f8e779957e2441c885548cbbeb15478
Ryan Luong <lumintriet@gmail.com>
2026-09-21 16:45:04 +0700

    feat: define lib entry.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+export * from "@/errors/invalid-json-string";
+export * from "@/errors/invalid-json-value";
+
+export * from "@/ops/is-json-value";
+export * from "@/ops/parse-json-string";
+export * from "@/ops/stringify-json-value";
+export * from "@/ops/stringify-unknown-value";
